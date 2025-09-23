c8760e10f2e8fa9f3a3b3fbb7b11067dfed9d8fd
BenjiBinge <benjamin@oijordsbakken.no>
2025-09-23 15:44:07 +0200

    no square, added click script + main button

---
+++ Assets/Scripts/Click.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    //Variables for the potatoes + currency
+    public int potatoes;
+    public int peeledPotatoes;
+    public int potates;
+
+
+    public void Peel()
+    {
+        peeledPotatoes++;
+    }
+}
