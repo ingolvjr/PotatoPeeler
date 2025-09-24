0bfbd968e51c9562c3696e7bd6e31e62a29d0329
Frederik I.L. Friquin <frederik.friquin@gmail.com>
2025-09-24 22:32:06 +0200

    Implemented UI in SampleScene and baseline potato-generation

--- Assets/Scripts/Click.cs
+++ Assets/Scripts/Click.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Click : MonoBehaviour
@@ -10,6 +12,25 @@ public class Click : MonoBehaviour
 
     public void Peel()
     {
-        peeledPotatoes++;
+        if (potatoes > 0)
+        {
+            peeledPotatoes++;
+            potatoes--;
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(_growPotatoes());
+    }
+
+    private IEnumerator _growPotatoes()
+    {
+        while (true)
+        {
+
+            yield return new WaitForSeconds(1);
+            potatoes++;
+        }
     }
 }
