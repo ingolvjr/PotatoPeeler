5468b4bca3497aecb6f98b771c2fa64d663dd52a
BenjiBinge <benjamin@oijordsbakken.no>
2025-09-24 10:50:48 +0200

    added Farmhand script

---
+++ Assets/Scripts/Farmhand.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using UnityEngine;
+
+public class Farmhand : MonoBehaviour
+{
+   
+    public float potatoes;
+    private Click _click;
+    
+    //Farmhand Grower
+    public bool farmhandGrowerHired;
+    public int farmhandAmount;
+    private float _farmhandGrowerTimer = 5f;
+    
+    //Farmhand Peeler
+    public bool farmhandPeelerHired;
+    public int farmhandPeelerAmount;
+    private float _farmhandPeelerTimer = 5f;
+    
+    //Potato seller
+    public bool potatoSellerHired;
+    public int potatoSellerAmount;
+    public int potatoSellerEarn = 15;
+    private float _potatoSellerTimer = 10f;
+
+
+
+    private void Start()
+    {
+        _click = GetComponent<Click>();
+    }
+
+    private void Update()
+    {
+        StartCoroutine(FarmhandGrower());
+        
+        StartCoroutine(FarmhandPeeler());
+        
+        StartCoroutine(PotatoSeller());
+        
+    }
+
+    //Farmhand grower grows potatoes
+    private IEnumerator FarmhandGrower()
+    {
+        while (farmhandGrowerHired)
+        {
+            yield return new WaitForSeconds(_farmhandGrowerTimer);
+            potatoes++;
+        }
+    }
+
+    //Farmhand peeler peels potatoes
+    private IEnumerator FarmhandPeeler()
+    {
+        while (farmhandPeelerHired)
+        {
+            yield return new WaitForSeconds(_farmhandPeelerTimer);
+            _click.peeledPotatoes += farmhandPeelerAmount;
+        }
+    }
+    
+    //Potato seller sells potatoes
+    private IEnumerator PotatoSeller()
+    {
+        while (potatoSellerHired)
+        {
+            yield return new WaitForSeconds(_potatoSellerTimer);
+            _click.potates += (potatoSellerEarn * potatoSellerAmount);
+        }
+    }
+}
